553af82635cb75efb85e835436aa82c38c0555eb
Paulo Valadares <paulo.valadares@alticeusa.com>
2022-09-04 19:58:25 +0100

    Express middleware

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } if (req.accepts('json')) {
+    } else if (req.accepts('json')) {
         res.json({ error: '404 not found' })
     } else {
       res.type('txt').send('404 not found');  
