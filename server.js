56e9acfcd6cfce61fdcb8d5ce6787af040f144cd
Paulo Valadares <paulo.valadares@alticeusa.com>
2022-09-04 20:50:57 +0100

    Express routes api

--- server.js
+++ server.js
@@ -23,57 +23,25 @@ const corsOptions = {
 }
 // app.use(cors()) // allow all
 app.use(cors(corsOptions));
+
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('^/$|/index(.html)?', (req, res) => {
-    // res.send('Hello World!!!')
-    // res.sendFile('./views/index.html', {root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
+// app.use(express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
-app.get('/new-page(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, 'new-page.html') // 302 by default
-})
-
-// route handler
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log('Attempted to load hello.html');
-    next()
-}, (req, res) => {
-    res.send('HELLO WORLD!')
-})
-
-
-const one = (req, res, next) => { 
-    console.log('try one...');
-    next()
-}
-
-const two = (req, res, next) => { 
-    console.log('try two...');
-    next()
-}
-
-const three = (req, res, next) => { 
-    console.log('try three...');
-    res.send('Finished!')
-}
-
-app.get('/chain(.html)?', [one, two, three])
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
 
 // app.all('/*(.html)?', (req, res) => {
 app.all('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
-        res.sendFile(path.join(__dirname, 'views', '404.html'))
-    } if (req.accepts('json')) {
-        res.json({ error: '404 not found' })
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ "error": '404 not found' })
     } else {
       res.type('txt').send('404 not found');  
     }
