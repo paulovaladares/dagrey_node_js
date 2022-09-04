597fac0ba080650753998688a4a4536809ab6e72
Paulo Valadares <paulo.valadares@alticeusa.com>
2022-09-04 19:25:56 +0100

    Express middleware

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
 
 const app = express();
+
+// Custom middleware LOGGER
+app.use(logger);
+
+const whiteList = ['https://www.yoursite.com', 'https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (originCall, callback) => {
+        if (whiteList.indexOf(originCall) !== -1 || !originCall) {
+            callback(null, true); // no error, can access
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+// app.use(cors()) // allow all
+app.use(cors(corsOptions));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     // res.send('Hello World!!!')
@@ -44,8 +67,18 @@ const three = (req, res, next) => {
 
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+// app.all('/*(.html)?', (req, res) => {
+app.all('*', (req, res) => {
+    res.status(404);
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } if (req.accepts('json')) {
+        res.json({ error: '404 not found' })
+    } else {
+      res.type('txt').send('404 not found');  
+    }
 })
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
